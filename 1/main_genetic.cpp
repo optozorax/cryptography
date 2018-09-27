adf004992c7cce1ced25a4c550cd3bddaaeb072a
Sheprut Ilya <optozorax@gmail.com>
2018-09-27 22:11:59 +0700

    Fix

--- 1/main_genetic.cpp
+++ 1/main_genetic.cpp
@@ -14,10 +14,10 @@ int main() {
 		if (text.size() == 0)
 			continue;
 
-		std::cout << "--------------------------------------------------------------------------------";
+		std::cout << "-------------------------------------------------------------------------------" << std::endl;
 		std::cout << "DECRYPT " << i+1 << " TEXT" << std::endl;
 
-		std::string bestKey = decryptByEvolution(text, false);
+		std::string bestKey = decryptByEvolution(text, true);
 		writeToFile("best_key_" + std::to_string(i+1) + ".txt", bestKey);
 
 		Cipher cipher;
